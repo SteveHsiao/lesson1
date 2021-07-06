2d22c86fc8f63b3466936008d85a4424af58ad12
Hutyr <32871477+SteveHsiao@users.noreply.github.com>
2021-07-06 11:46:29 +0800

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,12 @@
+grade = input ("Please enter your score:")
+grade = int(grade)
+if grade >= 90 and grade <= 100:
+    print ("Your grade is A")
+elif grade >= 80 and grade <= 89:
+    print ("Your grade is B")
+elif grade >= 70 and grade <= 79:
+    print ("Your grade is C")
+elif grade >= 60 and grade <= 69:
+    print ("Your grade is D")
+elif grade >= 0 and grade <= 59:
+    print ("Your grade is E") 
